fd5f3dc4aadc8bd73388d6db44311a5143e5244c
Jsmond2016 <Jsmond2016@gmail.com>
2020-11-27 15:49:41 +0800

    新增数据库查询

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -19,7 +19,7 @@ class HomeController extends Controller {
     let sql = 'SELECT article.id as id,'+
               'article.title as title,'+
               'article.introduce as introduce,'+
-              'article.add_time as addTime,'+
+              "FROM_UNIXTIME(article.add_time,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
               'article.view_count as viewCount ,'+
               '.type.type_name as typeName '+
               'FROM article LEFT JOIN type ON article.type_id = type.Id'
@@ -30,6 +30,31 @@ class HomeController extends Controller {
          data:results
      }
  }
+
+ async getArticleById(){
+  //先配置路由的动态传值，然后再接收值
+  let id = this.ctx.params.id
+
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  'article.introduce as introduce,'+
+  'article.article_content as articleContent,'+
+  "FROM_UNIXTIME(article.add_time,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
+  'article.view_count as viewCount ,'+
+  'type.type_name as typeName ,'+
+  'type.id as typeId '+
+  'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+  'WHERE article.id='+id
+
+
+
+  const result = await this.app.mysql.query(sql)
+
+
+  this.ctx.body={data:result}
+
+}
+
 }
 
 module.exports = HomeController;
