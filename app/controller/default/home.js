ff601be9d8bad34076c9f23a7e8cdaa8710a2407
Jsmond2016 <Jsmond2016@gmail.com>
2020-11-27 15:03:14 +0800

    完成数据库建表

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -13,6 +13,23 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, list';
   }
+
+  async getArticleList(){
+
+    let sql = 'SELECT article.id as id,'+
+              'article.title as title,'+
+              'article.introduce as introduce,'+
+              'article.add_time as addTime,'+
+              'article.view_count as viewCount ,'+
+              '.type.type_name as typeName '+
+              'FROM article LEFT JOIN type ON article.type_id = type.Id'
+ 
+     const results = await this.app.mysql.query(sql)
+ 
+     this.ctx.body={
+         data:results
+     }
+ }
 }
 
 module.exports = HomeController;
