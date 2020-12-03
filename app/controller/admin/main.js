fd3f3fc51f7845548b91f5ca5b6957baf1716e63
Jsmond2016 <Jsmond2016@gmail.com>
2020-12-03 14:59:20 +0800

    fix bugs

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -70,7 +70,6 @@ class MainController extends Controller {
 
   //获得文章列表
   async getArticleList() {
-
     let sql = 'SELECT article.id as id,' +
       'article.title as title,' +
       'article.introduce as introduce,' +
@@ -99,9 +98,9 @@ class MainController extends Controller {
       'article.title as title,' +
       'article.introduce as introduce,' +
       'article.article_content as article_content,' +
-      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime," +
+      "FROM_UNIXTIME(article.add_time,'%Y-%m-%d' ) as addTime," +
       'article.view_count as view_count ,' +
-      'type.typeName as typeName ,' +
+      'type.type_name as typeName ,' +
       'type.id as typeId ' +
       'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
       'WHERE article.id=' + id
